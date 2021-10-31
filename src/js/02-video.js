162bf5712077487817374790b99f76d87a067207
NataliGoIt <nataliakal4enko@gmail.com>
2021-10-31 10:45:54 +0200

    gallery, video, feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+player.on('timeupdate', throttle(onTimePlayer, 1000));
+function onTimePlayer(event) {
+  localStorage.setItem(STORAGE_KEY, `${event.seconds}`);
+}
+const savedTime = localStorage.getItem(STORAGE_KEY);
+player.setCurrentTime(savedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const LOCAL_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
+const formData = {};
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  let savedMessage = localStorage.getItem(LOCAL_KEY);
+  if (savedMessage) {
+    savedMessage = JSON.parse(savedMessage);
+    Object.entries(savedMessage).forEach(([name, value]) => {
+      refs.form.elements[name].value = value;
+      formData[name] = value;
+    });
+  }
+}
